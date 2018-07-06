cf0e94c39bdca7af9ea081ff67ab6a5a0672414f
Ion Talmaci <ntalmaci171@gmail.com>
2018-07-06 22:35:04 +0300

    adding parallax on scroll

--- source/js/main.js
+++ source/js/main.js
@@ -4,28 +4,76 @@
     
     //___________________/Parallax on mouse move/___________________//
     (function () {
-      var parallaxContainer = document.getElementById('parallax'),
-        layers = parallaxContainer.children;
+      if( $('.parallax')[0] ){
+          var parallaxContainer = document.getElementById('parallax'),
+          layers = parallaxContainer.children;
 
-      var moveLayers = function (e) {
-        var initialX = (window.innerWidth / 2) - e.pageX
-        initialY = (window.innerHeight / 2) - e.pageY;
+        var moveLayers = function (e) {
+          var initialX = (window.innerWidth / 2) - e.pageX
+          initialY = (window.innerHeight / 2) - e.pageY;
 
-        [].slice.call(layers).forEach(function (layer, i) {
-          var
-            divider = i / 100,
-            positionX = initialX * divider,
-            positionY = initialY * divider,
-            bottomPosition = (window.innerHeight / 2) * divider,
-            layerStyle = layer.style,
-            transformString = 'translate3d(' + positionX + 'px ,' + positionY + 'px , 0)';
+          [].slice.call(layers).forEach(function (layer, i) {
+            var
+              divider = i / 100,
+              positionX = initialX * divider,
+              positionY = initialY * divider,
+              layerStyle = layer.style,
+              transformString = 'translate3d(' + positionX + 'px ,' + positionY + 'px , 0)';
 
-          layerStyle.transform = transformString;
-          // layerStyle.bottom = '-' + bottomPosition + 'px';
-        });
+            layerStyle.transform = transformString;
+          });
+        }
+        window.addEventListener('mousemove', moveLayers);
       }
-      window.addEventListener('mousemove', moveLayers);
+
     })();
 
+    //___________________/Parallax on scroll positive/___________________//
+      var parallax = (function(){
+        var bg            = document.querySelector('.hero__bg');
+
+        return{
+          move: function (block, windowScroll, strafeAmmount){
+              var strafe          = windowScroll / strafeAmmount + '%';
+              var style           = block.style;
+              var transformString = 'translate3d(0, ' + strafe + ' , 0)';
+
+              style.transform = transformString;
+              style.webkitTransform = transformString;
+          },
+
+          init: function(wScroll){
+              this.move(bg, wScroll, 30);
+          }
+        }
+      }());
+
+      //___________________/Parallax on scroll negative/___________________//
+      var parallaxNegative = (function(){
+        var user          = document.querySelector('.hero__user-block'),
+            sectionTitle  = document.querySelector('.section__title-header');
+
+        return{
+          move: function (block, windowScroll, strafeAmmount){
+              var strafe          = windowScroll / -strafeAmmount + '%';
+              var style           = block.style;
+              var transformString = 'translate3d(0, ' + strafe + ' , 0)';
+
+              style.transform = transformString;
+              style.webkitTransform = transformString;
+          },
+
+          init: function(wScroll){
+              this.move(user, wScroll, 5);
+              this.move(sectionTitle, wScroll, 15);
+          }
+        }
+      }());
+
+    window.onscroll = function(){
+      var wScroll = window.pageYOffset;
+      parallax.init(wScroll);
+      parallaxNegative.init(wScroll);
+    }
   })
 })(jQuery);
